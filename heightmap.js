db370ad5f001c93928db637961e10a93ddbb17d8
opzmetek <op.zmetek@gmail.com>
2026-03-27 19:08:42 +0100

    Refactor Heightmap class methods for clarity

--- heightmap.js
+++ heightmap.js
@@ -1,23 +1,23 @@
-export default class Heightmap{
-  constructor(map,xl,yl){
-    this.map = map?map:new Float32Array(xl*yl);
+export default class Heightmap {
+  constructor(map, xl, yl) {
+    this.map = map ? map : new Float32Array(xl * yl);
     this.xLen = xl;
     this.yLen = yl;
-    this.xCenter = xl*0.5;
-    this.yCenter = yl*0.5;
+    this.xCenter = xl * 0.5;
+    this.yCenter = yl * 0.5;
   }
-  
-  get(x,y){
-    const xx = this.xCenter+x;
-    const yy = this.yCenter+y;
-    if(xx>=this.xLen||yy>=this.yLen)return 0;
-    return this.map[xx*this.xLen+yy];
+
+  get(x, y) {
+    const xx = Math.floor(this.xCenter + x);
+    const yy = Math.floor(this.yCenter + y);
+    if (xx < 0 || xx >= this.xLen || yy < 0 || yy >= this.yLen) return 0;
+    return this.map[yy * this.xLen + xx]; // správně řádek*width + sloupec
   }
-  
-  set(x,y,value){
-    const xx = this.xCenter+x;
-    const yy = this.yCenter+y;
-    if(xx>=this.xLen||yy>=this.yLen)return;
-    this.map[xx*this.xLen+yy] = value;
+
+  set(x, y, value) {
+    const xx = Math.floor(this.xCenter + x);
+    const yy = Math.floor(this.yCenter + y);
+    if (xx < 0 || xx >= this.xLen || yy < 0 || yy >= this.yLen) return;
+    this.map[yy * this.xLen + xx] = value; // správně řádek*width + sloupec
   }
 }
