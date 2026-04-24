e2029dbff824ef88fd611660e6fcbf9d803dd722
opzmetek <op.zmetek@gmail.com>
2026-04-24 21:08:41 +0200

    Remove game scene and camera initialization
    Removed initialization of the game scene and camera setup.

--- app.js
+++ app.js
@@ -15,20 +15,9 @@ import {initAudio, analyse} from "/TheVertexFiles/music/audio.js";
 import {moveStep, dash, anchor} from "/TheVertexFiles/core/player/move.js";
 import {setupScene, setupFlags, initLevel, initUtil} from "/TheVertexFiles/core/boot.js";
 
+setupScene();
 Game.urlParams = new URLSearchParams(window.location.search);
-
 Game.sensivity = 0.02;
-World.scene = new THREE.Scene();
-Game.renderer = new THREE.WebGLRenderer();
-Game.renderer.setSize(window.innerWidth, window.innerHeight);
-Game.camera = new THREE.PerspectiveCamera(60,window.innerWidth/window.innerHeight,0.1,100);
-World.pitch = new THREE.Object3D();
-World.yaw = new THREE.Object3D();
-World.yaw.add(World.pitch);
-World.yaw.position.set(-2,0,-2);
-World.pitch.add(Game.camera);
-World.scene.add(World.yaw);
-World.pitch.position.y+=2;
 Game.mobile = "ontouchstart" in window||navigator.maxTouchPoints>0||Game.urlParams.get("Game.mobile")==="true";
 
 export function startGame(tId,lId){
