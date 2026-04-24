f2c2de168ac7dd3c1e72ccdc9d9cb7b3a0ea7c75
opzmetek <op.zmetek@gmail.com>
2026-04-24 21:13:16 +0200

    Initialize scene setup with WebGL renderer and camera

--- core/boot.js
+++ core/boot.js
@@ -1,1 +1,18 @@
+import {Scene, WebGLRenderer, PerspectiveCamera, Object3D} from "/TheVertexFiles/three.module.js";
+import {World, Game} from "/TheVertexFiles/core/state.js";
 
+const PLAYER_HEIGHT = 2;
+
+export function setupScene(){ 
+  World.scene = new THREE.Scene();
+  Game.renderer = new THREE.WebGLRenderer();
+  Game.renderer.setSize(window.innerWidth, window.innerHeight);
+  Game.camera = new THREE.PerspectiveCamera(60,window.innerWidth/window.innerHeight,0.1,100);
+  World.pitch = new THREE.Object3D();
+  World.yaw = new THREE.Object3D();
+  World.yaw.add(World.pitch);
+  World.yaw.position.set(-2,0,-2);
+  World.pitch.add(Game.camera);
+  World.scene.add(World.yaw);
+  World.pitch.position.y+=PLAYER_HEIGHT;
+}
