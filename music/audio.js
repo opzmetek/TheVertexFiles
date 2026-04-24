029aa6fa360ff5699f599150129ce5b90be4f69d
opzmetek <op.zmetek@gmail.com>
2026-04-24 19:40:49 +0200

    Implement audio analysis function
    Add audio analysis function to compute energy based on audio data.

--- music/audio.js
+++ music/audio.js
@@ -1,1 +1,12 @@
+import {World, Audio} from "/TheVertexFiles/core/state.js";
 
+function analyse() {
+  Audio.analyser.getByteTimeDomainData(Audio.bin);
+  let sum = 0;
+  for(let i=0;i<Audio.bin.length;i++){
+    sum+=bin[i];
+  }
+  const avg = sum/Audio.bin.length;
+  const energy = avg / 255;
+  World.mesh.material.uniforms.thickness.value = (1.0+energy * energy * 2.0) * Game.lWidth;
+}
