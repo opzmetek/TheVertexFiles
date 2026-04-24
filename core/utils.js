b196e0575aeee1272dc66f9a8d3be0d08b9dea00
opzmetek <op.zmetek@gmail.com>
2026-04-24 17:04:22 +0200

    Add utility functions in utils.js
    Implement utility functions for loading and manipulating objects.

---
+++ core/utils.js
@@ -0,0 +1,39 @@
+import {World} from "/core/state.js";
+import {importVrx} from "/io.js";
+import {Group} from "/three.module.js";
+
+function getByPath(obj,path,stepsUp=0){
+  const p=path.split('.');
+  return (stepsUp>0?p.slice(0,-stepsUp):p).reduce((acc, key) => acc?.[key], obj);
+}
+
+export function rnd(min,max){
+  return min+Math.floor(Math.random()*(max-min));
+}
+
+export function di(n){
+  return document.getElementById(n);
+}
+
+export async function loadAll(arr,loader,prefix = "",suffix = ""){
+  const objects = [];
+  for(const o of arr){
+    const array = (await loadOne(prefix+o+suffix, loader));
+    const group = new Group();
+    array.forEach(m=>group.add(m));
+    objects[o] = group;
+  }
+  return objects;
+}
+
+export async function loadOne(o,loader){
+  if(loader)loader.textContent=("From "+o);
+  return await importVRX(o);
+}
+
+export function remove(m){
+  World.scene.remove(m);
+  if(m.material)m.material.dispatch();
+  if(m.geometry)m.geometry.dispatch();
+  if(m.childern)m.childern.forEach(c=>remove(c));
+}
