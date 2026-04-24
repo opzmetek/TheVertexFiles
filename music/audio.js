5e6b4cc8838f993330972483269981b43f22c0a8
opzmetek <op.zmetek@gmail.com>
2026-04-24 20:07:21 +0200

    Export analyse function and update initAudio

--- music/audio.js
+++ music/audio.js
@@ -1,6 +1,6 @@
 import {World, Audio} from "/TheVertexFiles/core/state.js";
 
-function analyse() {
+export function analyse() {
   Audio.analyser.getByteTimeDomainData(Audio.bin);
   let sum = 0;
   for(let i=0;i<Audio.bin.length;i++){
@@ -9,4 +9,22 @@ function analyse() {
   const avg = sum/Audio.bin.length;
   const energy = avg / 255;
   World.mesh.material.uniforms.thickness.value = (1.0+energy * energy * 2.0) * Game.lWidth;
+}
+
+
+
+export async function initAudio(name){
+  Audio.audioCtx = new (window.AudioContext||window.WebkitAudioContext)();
+  Audio.analyser = Audio.audioCtx.createAnalyser();
+  Audio.analyser.fftSize = 32;
+  const res = await fetch("./music/"+name);
+  const buff = await res.arrayBuffer();
+  console.log(res, buff);
+  const buffer = await audioCtx.decodeAudioData(buff);
+  Audio.source = audioCtx.createBufferSource();
+  Audio.source.buffer = buffer;
+  Audio.source.connect(analyser);
+  Audio.analyser.connect(audioCtx.destination);
+  Audio.bin = new Uint8Array(analyser.frequencyBinCount);
+  Audio.source.start();
 }
