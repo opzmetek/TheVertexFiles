5a707c65a69e765e1433610ce38f69a5f79601ec
opzmetek <op.zmetek@gmail.com>
2026-04-24 20:11:16 +0200

    Refactor player movement functions
    Removed old movement logic and added new movement functions.

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ import {EnemyAI, StaticTargetAI, aiTypes} from "/TheVertexFiles/ai/enemy_ai.js";
 import {Enemy} from "/TheVertexFiles/core/enemy.js";
 import {di, remove, loadOne, loadAll, rnd, getByPath} from "/TheVertexFiles/core/utils.js";
 import {initAudio, analyse} from "/TheVertexFiles/music/audio.js";
+import {moveStep, move} from "/TheVertexFiles/core/player/move.js";
 
 const UP = new THREE.Vector3(0, 1, 0);
 Game.urlParams = new URLSearchParams(window.location.search);
@@ -175,54 +176,7 @@ export function startGame(tId,lId){
   
   const vFor = new THREE.Vector3();
 
-  function moveStep(dt){
-    const stepSize = Player.speed * dt;
-    const MAX_STEP = 0.3;
-    const steps = Math.ceil(stepSize / MAX_STEP);
-    const stepDt = dt / steps;
-    for (let i = 0; i < steps; i++) {
-      move(stepDt);
-    }
-  }
-
-  function move(dt) {
-    Game.camera.getWorldDirection(vFor);
-    vFor.y = 0;
-    vFor.normalize();
-    let len = Math.hypot(Game.input.mx, Game.input.my);
-    let inputX = len > 1 ? Game.input.mx / len : Game.input.mx;
-    let inputZ = len > 1 ? Game.input.my / len : Game.input.my;
-    const targetVX = (inputX * vFor.x + inputZ * -vFor.z) * Player.speed;
-    const targetVZ = (inputX * vFor.z + inputZ *  vFor.x) * Player.speed;
-    const t = Math.min(dt * 4, 1);
-    Player.velocityX = Math.min(Player.speed, Player.velocityX + (targetVX - Player.velocityX) * t);
-    Player.velocityY = Math.min(Player.speed, Player.velocityY + (targetVZ - Player.velocityY) * t);
-    if (len === 0) {
-      Player.velocityX *= Math.pow(0.8, dt * 60);
-      Player.velocityY *= Math.pow(0.8, dt * 60);
-    }
-    let x = World.yaw.position.x;
-    let z = World.yaw.position.z;
-    let y = World.yaw.position.y;
-    let nx = x + Player.velocityX * t;
-    if (!checkCollisionXZ(nx, z, y)) nx = x;
-    let nz = z + Player.velocityY * t;
-    if (!checkCollisionXZ(nx, nz, y)) nz = z;
-    x = nx;
-    z = nz;
-    Player.vertVec -= Game.gravity * dt;
-    y += Player.vertVec * dt;
-    const floorH = getMaxFloor(x, z);
-    if (y <= floorH) {
-      y = floorH;
-      Player.vertVec = 0;
-      Player.onGround = true;
-    } else {
-      Player.onGround = false;
-    }
-    World.yaw.position.set(x, y, z);
-  }
-
+  
   function dash(){
     if(performance.now()-Game.timers.dash<PlayerConfig.dashDelay)return;
     Game.timers.dash = performance.now();
