7c11d0c01cbff7c5c86225590a27586042cd4fcd
opzmetek <op.zmetek@gmail.com>
2026-04-24 16:26:01 +0200

    Remove GridMaterial function and shader code
    Removed the GridMaterial function and its associated shader code.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import {importVRX,importHeightmap} from './io.js';
 import {Game, World, Audio, Player} from "/core/state.js";
 import {gameUI, loadUI, escape, loadGame} from "/ui/ui.js";
 import {createStartingPanel} from "/ui/start.js";
+import {GridMaterial} from "/core/shader.js";
 
 //custom raycast
 const raycaster = new THREE.Raycaster();
@@ -645,69 +646,6 @@ function remove(m){
 loadGame();
 window.onload=loadUI;
 
-const GridMaterial = (bc = 0x000000,lineColor = 0xffa500,squareSize = 2.5) => new THREE.ShaderMaterial({
-  uniforms: {
-    lineColor: { value: new THREE.Color(lineColor) },
-    squareSize: { value: squareSize },
-    thickness: { value: 1.0 },
-    baseColor: { value: new THREE.Color(bc) },
-  },
-  side: THREE.DoubleSide,
-  vertexShader: `
-    varying vec3 vWorldPos;
-    void main() {
-      vWorldPos = (modelMatrix * vec4(position, 1.0)).xyz;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    }
-  `,
-  fragmentShader: `
-precision highp float;
-uniform float squareSize;
-uniform float thickness;
-uniform vec3 lineColor;
-uniform vec3 baseColor;
-varying vec3 vWorldPos;
-
-void main() 
-{
-    vec3 pos = vWorldPos;
-    vec3 color = lineColor;
-
-    // --- derivace pro anti-aliasing ---
-    float dXx = abs(dFdx(pos.x));
-    float dYx = abs(dFdy(pos.x));
-    float dXy = abs(dFdx(pos.y));
-    float dYy = abs(dFdy(pos.y));
-    float dXz = abs(dFdx(pos.z));
-    float dYz = abs(dFdy(pos.z));
-
-    float widthX = max(dXx, dYx) * 2.5 * thickness;
-    float widthY = max(dXy, dYy) * 2.5 * thickness;
-    float widthZ = max(dXz, dYz) * 2.5 * thickness;
-
-    float alpha = 0.0;
-
-    // --- osa X (linky v rovině YZ) ---
-    float rem = mod(pos.x, squareSize);
-    alpha += smoothstep(widthX,0.0, rem);
-    alpha += smoothstep(widthX,0.0, squareSize - rem);
-
-    // --- osa Y (linky v rovině XZ) ---
-    rem = mod(pos.y, squareSize);
-    alpha += smoothstep(widthY,0.0, rem);
-    alpha += smoothstep(widthY,0.0, squareSize - rem);
-
-    // --- osa Z (linky v rovině XY) ---
-    rem = mod(pos.z, squareSize);
-    alpha += smoothstep(widthZ,0.0, rem);
-    alpha += smoothstep(widthZ,0.0, squareSize - rem);
-
-    alpha = clamp(alpha, 0.0, 1.0);
-
-    gl_FragColor = vec4(mix(baseColor,color, alpha),1.0);
-}
-  `,
-});
 
 
 
