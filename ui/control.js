a8d77158756a34b58e39e786ec07f0760f13686e
opzmetek <op.zmetek@gmail.com>
2026-04-28 20:33:36 +0200

    Add new key codes for escape, dash, anchor, and sprint

--- ui/control.js
+++ ui/control.js
@@ -1,1 +1,1 @@
-export const keyCodes = {moveLeft:"a",moveRight:"d",moveFront:"w",moveBack:"s",jump:" "};
+export const keyCodes = {moveLeft:"a",moveRight:"d",moveFront:"w",moveBack:"s",jump:" ", escape:"escape", dash: "x", anchor:"e", sprint:"shift"};
