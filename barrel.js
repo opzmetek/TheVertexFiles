afcfdd169976400762e41457e8855c6d8bb26b81
opzmetek <op.zmetek@gmail.com>
2026-04-29 18:42:39 +0200

    Add barrel.js for exporting core modules

---
+++ barrel.js
@@ -0,0 +1,6 @@
+export {Game, World, Player, PlayerConfig, Audio} from "/TheVertexFiles/core/state.js";
+export {DDARaycast} from "/TheVertexFiles/core/raycast.js";
+export {FastAStar} from "/TheVertexFiles/ai/astar.js";
+export {spawn, spawnEnemy} from "/TheVertexFiles/core/enemy/spawn.js";
+export {Enemy} from "/TheVertexFiles/core/enemy/enemy.js";
+export {move, moveStep, dash, anchor} from "/TheVertexFiles/core/player/move.js";
