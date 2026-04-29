9b8189bb355eafcffaf5ae54f7de98ded424be0d
opzmetek <op.zmetek@gmail.com>
2026-04-29 20:10:10 +0200

    Update shoot.js

--- core/player/shoot.js
+++ core/player/shoot.js
@@ -33,5 +33,9 @@ export function shoot(pos, dir, speed){
 }
 
 export function updateBullets(dt){
-  
+  World.bullets.forEach(b=>{
+    b.p.add(b.dir.multiplyScalar(dt));
+    b.t += dt;
+    if(b.t>=b.maxT)b.m.material.visible = false;
+  });
 }
