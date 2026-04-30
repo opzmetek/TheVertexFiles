005dec7141c7ced9f4e065521f2b41c1f379bce6
opzmetek <op.zmetek@gmail.com>
2026-04-30 09:17:03 +0200

    Update mixins.js

--- ai/mixins.js
+++ ai/mixins.js
@@ -2,6 +2,9 @@ export function MeeleeMixin(base){
   return class extends base{
     move(dt){
       super.move(dt);
+      if(this.temp.subVectors(this.target, this.enemy.p).lengthSq()>this.attackDist*this.attackDist){
+        
+      }
     }
   }
 }
