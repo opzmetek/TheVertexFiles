84b6b848b0ae29477e2370502fff24adfdb2a07a
opzmetek <op.zmetek@gmail.com>
2026-05-01 09:31:49 +0200

    Update raycast.js and debug

--- core/raycast.js
+++ core/raycast.js
@@ -12,6 +12,7 @@ export function DDARaycast(mesh, ray, near=0, far=Infinity){
   const cellSize = 1;
   let cx = Math.floor(ox / cellSize);
   let cz = Math.floor(oz / cellSize);
+  console.log("ox, oy, oz, cx, cz, h: ",ox, oy, oz, cx, cz, mesh.heightmap.get(cz, cx));
   const stepX = dx > 0 ? 1 : -1;
   const stepZ = dz > 0 ? 1 : -1;
   const tDeltaX = dx !== 0 ? cellSize / Math.abs(dx) : Infinity;
@@ -20,7 +21,8 @@ export function DDARaycast(mesh, ray, near=0, far=Infinity){
   let tMaxZ = dz > 0 ? ((cz + 1) * cellSize - oz) / dz : dz < 0 ? (cz * cellSize - oz) / dz : Infinity;
   let t = 0;
   while (t < far) {
-    const h = mesh.heightmap.get(cx,cz);
+    const h = mesh.heightmap.get(cz,cx);
+    console.log("cx, cz, h: ", cx, cz, h);
     if (h !== undefined) {
       const yHit = oy + dy * t;
       if (yHit <= h && t > near) {
