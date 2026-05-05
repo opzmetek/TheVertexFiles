7e4cc2f45b0898e20c4f679860c8bcdae9b741e7
opzmetek <op.zmetek@gmail.com>
2026-05-05 14:42:25 +0200

    Update particle.js

--- core/particle.js
+++ core/particle.js
@@ -1,11 +1,29 @@
-import {PlaneGeometry, InstancedMesh} from "three";
+import {PlaneGeometry, InstancedMesh, Vector3} from "three";
 import {Game, World, BillboardMaterial} from "barrel";
 
+const MAX = 1000;
+const UP = new Vector3(0, 1, 0);
+const arr = new Float32Array(MAX * 7);//pos(xyz), vel(xyz), life
 let particles;
+let start = 0, end = 0;
 
 export function initParticles(){
   const geo = new PlaneGeometry(0.05,0.05);
   const mat = new BillboardMaterial(World.colors[0]);
-  particles = new InstancedMesh(geo, mat, 1000);
+  particles = new InstancedMesh(geo, mat, MAX);
   World.scene.add(particles);
+}
+
+export function explode(pos, dir, count){
+  const forward = dir.clone();
+  const right = new Vector3().crossVectors(forward, UP);
+  const up = new Vector3().crossVectors(forward, right);
+  const dummy = new Vector3();
+  for(let i = 0; i < count; i++){
+    const idx = (end + i) % MAX;
+    const rx = Math.random() * 2 - 1;
+    const ry = Math.random() * 2 - 1;
+    const rz = Math.random();
+    
+  }
 }
