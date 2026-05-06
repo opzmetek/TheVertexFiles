5568cb674d1655eff01969f34983673ab64a19d2
opzmetek <op.zmetek@gmail.com>
2026-05-06 17:38:15 +0200

    Update particle.js fix

--- core/particle.js
+++ core/particle.js
@@ -8,7 +8,7 @@ let tmp;
 export function initParticles(){
   const count = 40;
   const goldenAngle = Math.PI * (3 - Math.sqrt(5));
-  const positions = new Float32Array(count);
+  const positions = new Float32Array(count * 3);
   for(let i = 0; i<count; i++){
     const d = i * 3;
     const t = i/(count-1);
@@ -25,19 +25,17 @@ export function initParticles(){
   geo.setAttribute("position", new Float32BufferAttribute(positions, 3));
   const mat = new PointsMaterial({
     color: World.colors[0],
-    transparent: true,
-    blending: AdditiveBlending,
-    size: 0.1
+    size: 0.5
   });
   tmp = new Points(geo, mat);
 }
 
 export function explode(pos, dir, size, speed, life){
+  if(!tmp) initParticles();
   const emitter = tmp.clone();
   emitter.life = life;
   emitter.position.copy(pos);
   emitter.quaternion.setFromUnitVectors(UP, dir);
-  emitter.scale.setScalar(0);
   emitter.speed = speed;
   emitter.maxLife = life;
   emitter.size = size;
@@ -49,7 +47,10 @@ export function updateParticles(dt){
   for(let i = particles.length-1; i >= 0; i--){
     const p = particles[i];
     p.life -= dt;
-    if(p.life<=0) particles[i] = particles.pop();//swap remove
+    if(p.life<=0){
+      particles[i] = particles[particles.length - 1];
+      particles.pop();
+    }
     p.scale.setScalar((1-(p.life/p.maxLife)) * p.size);
   }
 }
