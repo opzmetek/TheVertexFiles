ce86557dfcae44deeed90b5b803a5fdc00776c17
opzmetek <op.zmetek@gmail.com>
2026-05-10 17:00:56 +0200

    Refactor event system to use static class property

--- core/ecosystem.js
+++ core/ecosystem.js
@@ -1,14 +1,18 @@
-const listeners = {};
+class X {
+  static listeners = {}
+}
 
 export function on(name, callback){
+  const listeners = X.listeners;
   if(listeners[name])listeners[name].push(callback);
   else listeners[name] = [callback];
 }
 
 export function emit(name, ...args){
+  const listeners = X.listeners;
   listeners[name]?.forEach(l=>l(...args));
 }
 
 export function resetEventSystem(){
-  listeners = {};
+  X.listeners = {};
 }
