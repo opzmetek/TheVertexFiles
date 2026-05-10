b1fadbab434c171fd6b10329a10a4de4d06fe03d
opzmetek <op.zmetek@gmail.com>
2026-05-10 17:06:55 +0200

    Refactor event system to use standalone listeners
    Refactor event system to use a standalone listeners object instead of a class.

--- core/ecosystem.js
+++ core/ecosystem.js
@@ -1,18 +1,14 @@
-class X {
-  static listeners = {}
-}
+const listeners = {};
 
 export function on(name, callback){
-  const listeners = X.listeners;
   if(listeners[name])listeners[name].push(callback);
   else listeners[name] = [callback];
 }
 
 export function emit(name, ...args){
-  const listeners = X.listeners;
   listeners[name]?.forEach(l=>l(...args));
 }
 
 export function resetEventSystem(){
-  X.listeners = {};
+  listeners = {};
 }
