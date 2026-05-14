0ad2815182f1cb8822287b16be4a484a23ed0022
opzmetek <op.zmetek@gmail.com>
2026-05-14 09:14:12 +0200

    Add HorizontalScrollable custom element with scrolling

--- elements.js
+++ elements.js
@@ -59,12 +59,15 @@ class SliderSwitch extends HTMLElement {
           content: "";
 
           position: absolute;
+          left: 0px;
+          top: 0px;
 
           width: var(--width);
           height: var(--height);
           clip-path: var(--hex);
           
           background: var(--thumb);
+          border-radius: 5px;
 
           transition: transform .25s ease;
 
@@ -127,7 +130,94 @@ class SliderSwitch extends HTMLElement {
   }
 }
 
+class HorizontalScrollable extends HTMLElement {
+  constructor(){
+    super();
+
+    this.attachShadow({mode: "open"});
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          overflow: hidden;
+        }
+      
+        .moveleft, .moveright 
+          width: 50px;
+          height: 80px;
+          position: absolute;
+          top: 50%;
+          transform: translateY(-50%);
+        }
+
+        .moveleft {
+          left: 10px;
+        }
+
+        .moveright {
+          right: 10px;
+        }
+
+        .childRoot {
+          height: 100%;
+          position: absolute;
+          left: 0px;
+          top: 0px;
+          overflow: hidden;
+        }
+      </style>
+
+      <button class = ".moveleft></button>
+      <button class = ".moveright></button>
+    `;
+
+    this.childRoot = document.createElement("div");
+  }
+
+  connectedCallback() {
+    super.appendChild(this.childRoot);
+
+    let down = false, lx = 0;
+
+    this.addEventListener("pointerdown", e=>{
+      down = true;
+      lx = e.clientX;
+    });
+    
+    this.addEventListener("pointermove", e=>{
+      if(!down) return;
+      const x = e.clientX;
+      const dx = x - lx;
+      lx = x;
+      this.childRoot.scrollBy(dx);
+    });
+
+    this.addEventListener("pointerup", e=>{
+      down = false;
+    });
+
+    const left = this.querySelector(".moveleft");
+    const right = this.querySelector(".moveright");
+
+    left.onclick = e=>{
+      e.preventDefault();
+      e.stopPropagation();
+      this.childRoot.scrollBy({left: 200, behavior: "smooth"});
+    };
+
+    right.onclick = e=>{
+      e.preventDefault();
+      e.stopPropagation();
+      this.childRoot.scrollBy({left: -200, behavior: "smooth"});
+    };
+  }
+}
+
 customElements.define(
   "s-switch",
   SliderSwitch
+);
+
+customElements.define(
+  "horizontal-scrollable",
+  HorizontalScrollable
 );
