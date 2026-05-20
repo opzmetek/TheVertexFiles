b07f3fb56f09eaa73e870233bfb6b01e0bf5eaf4
opzmetek <op.zmetek@gmail.com>
2026-05-20 14:53:18 +0200

    Refactor enemy AI movement and vertical update logic

--- ai/enemy_ai.js
+++ ai/enemy_ai.js
@@ -11,6 +11,7 @@ export class EnemyAI{
     this.desired = new Vector3();
     this.vel = new Vector3();
     this.easing = 2;
+    this.vertical = 0;
   }
   computeSteering(dt, exp){
     return this.temp.copy(this.desired.lerp(this.temp.subVectors(this.target, this.enemy.p).normalize(), exp)).multiplyScalar(this.enemy.speed);
@@ -19,18 +20,32 @@ export class EnemyAI{
     this.desired.x += (Math.random()-0.5)*0.05;
     this.desired.z += (Math.random()-0.5)*0.05;
   }
+  updateVertical(dt){
+    let y = this.enemy.p.y;
+    y -= this.vertical * dt;
+    const f = this.getMaxFloor(this.enemy.p.x, this.enemy.p.z);
+    if(y<=f){
+      this.onGround = true;
+      this.enemy.p.y = f + 0.01;
+      this.vertical = 0;
+    } else {
+      this.enemy.p.y = y;
+      this.vertical -= Game.gravity * dt;
+    }
+  }
   move(dt, exp){
-    if(dt>0.4)dt=0.4;//spike
+    if(dt > 0.4)dt = 0.4;//spike
     const p = this.enemy.p;
     this.updateSteering(dt, exp);
     const dir = this.computeSteering(dt, exp);
-    this.vel.lerp(dir,dt*this.easing);
+    this.vel.lerp(dir, dt * this.easing);
     const y = p.y;
-    let nx = p.x+this.vel.x*dt, nz = p.z+this.vel.z*dt;
-    if(this.tryMove(nx, y, nz))return;
-    else if(this.tryMove(nx, y, p.z))return;//only x
-    else if(this.tryMove(p.x, y, nz))return;//only z
-    this.onStuck();
+    let nx = p.x + this.vel.x * dt, nz = p.z + this.vel.z * dt;
+    if(this.tryMove(nx, y, nz));
+    else if(this.tryMove(nx, y, p.z));//only x
+    else if(this.tryMove(p.x, y, nz));//only z
+    else this.onStuck();
+    this.updateVertical(dt);
   }
   onStuck(){
     this.desired.set(Math.random()-0.5, 0, Math.random()-0.5);
@@ -38,7 +53,8 @@ export class EnemyAI{
   tryMove(x,y,z){
     const f = this.getMaxFloor(x, z);
     if(f > y+0.2)return false;
-    this.enemy.p.set(x, f+0.001, z);
+    this.enemy.p.x = x;
+    this.enemy.p.z = z;
     return true;
   }
   getMaxFloor(px, pz) {
