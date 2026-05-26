b5f5411eaf40fb206aec17934d743aa02c10e0d2
opzmetek <op.zmetek@gmail.com>
2026-05-26 13:55:43 +0200

    Refactor shooting mechanics and bullet initialization

--- core/player/shoot.js
+++ core/player/shoot.js
@@ -1,7 +1,6 @@
-import {World, Game, Player, PlayerConfig, DDARaycast, Animation, raycaster, explode, on} from "barrel";
-import {BoxGeometry, MeshBasicMaterial, AdditiveBlending, Mesh, Vector3, Vector2, Ray} from "three";
+import {World, Game, Player, PlayerConfig, DDARaycast, Animation, raycaster, explode, on, initBullet} from "barrel";
+import {Vector3, Vector2, Ray} from "three";
 
-const Z_AXIS = new Vector3(0, 0, 1);
 const MIN_FOV = 4;
 let bulletTemplate;
 const direction = new Vector3();
@@ -77,17 +76,7 @@ function updateDirection(screen){
   direction.copy(raycaster.ray.direction);
 }
 
-function createBullet(){
-  if(bulletTemplate) return bulletTemplate.clone();
-  const geometry = new BoxGeometry(0.05, 0.05, 1);
-  const material = new MeshBasicMaterial({
-    color: World.colors[0],
-  });
-  bulletTemplate = new Mesh(geometry, material);
-  return bulletTemplate.clone();
-}
-
-function checkPlayerShoot(dt){
+export function checkPlayerShoot(dt){
   const max = 1/Player.gunType.fireRate;
   lastShootTime += dt;
   if(lastShootTime >= max&&playerShooting){
@@ -116,29 +105,7 @@ export function shoot(pos, speed){
   initBullet(dist, speed, dir, pos);
 }
 
-function initBullet(dist, speed, dir, pos){
-  const bullet = {vel: dir.clone().multiplyScalar(speed), t: 0};
-  bullet.maxT = dist/speed;
-  console.log(dist, bullet);
-  bullet.m = createBullet();
-  bullet.m.quaternion.setFromUnitVectors(Z_AXIS, dir);
-  bullet.p = bullet.m.position.copy(pos);
-  World.bullets.push(bullet);
-  World.scene.add(bullet.m);
-}
-
 const tempVec = new Vector3();
-
-function enemyHit(ray, len){
-  tempVec.subVectors(World.yaw.position, ray.origin);
-  const t = tempVec.dot(ray.direction);
-  if(t<0||t>len)return len;
-  const w = PlayerConfig.size, h = PlayerConfig.height;
-  if(tempVec.lengthSq()-t*t<w*w+w*w+h*h){
-    return t;
-  }
-  return len;
-}
 
 function testEnemies(ray, len, out){
   const sql = len*len;
@@ -162,19 +129,4 @@ function penetrateEnemies(enemies, penetration){
     if(penetration<=0)return e.shoot_testEnemies_dist;
   }
   return enemies[enemies.length - 1]?.shoot_testEnemies_dist;
-}
-
-export function updateBullets(dt){
-  checkPlayerShoot(dt);
-  for(let i = World.bullets.length-1;i>=0;i--){
-    const b = World.bullets[i];
-    b.p.addScaledVector(b.vel,dt);
-    b.t += dt;
-    if(b.t>=b.maxT){
-      explode(b.p, b.vel.normalize().negate(), 3, 0.2);
-      World.bullets[i] = World.bullets[World.bullets.length-1];
-      World.bullets.pop();
-      World.scene.remove(b.m);
-    }
-  }
 }
